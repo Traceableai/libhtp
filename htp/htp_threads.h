219e8e74232884404cb3d20ad30da6ef543bd275
Mayur Mahrotri <mayur.mahrotri@traceable.ai>
2023-04-05 18:09:26 -0400

    chore: add includes

--- htp/htp_threads.h
+++ htp/htp_threads.h
@@ -1,3 +1,17 @@
+#if HAVE_SYSCALL_H
+#include <syscall.h>
+#endif
+
+#if HAVE_SYS_TYPES_H
+#include <sys/types.h> /* for gettid(2) */
+#endif
+
+#if HAVE_SYS_PRCTL_H
+#include <sys/prctl.h>
+#endif
+
+#include <pthread.h>
+
 #ifndef OS_FREEBSD
 #define HTPGetThreadIdLong(...) ({ \
    pid_t tmpthid; \
